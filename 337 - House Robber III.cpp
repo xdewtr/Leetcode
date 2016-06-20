f974e4ad2221b341d09791832bbe5d432686ec0d
Chia-Lung Wu <KevinWu@Chia-Lungs-MacBook-Air.local>
2016-06-19 17:18:01 -0700

    337 completed

--- 337 - House Robber III.cpp
+++ 337 - House Robber III.cpp
@@ -10,28 +10,24 @@
 class Solution {
 public:
     int rob(TreeNode* r) {
-        queue<TreeNode*> qu;
-        int sumodd =0, sumeven =0, tmp=0;
-        bool tick =1;
         if(!r) return 0;
-        qu.push(r);
-        qu.push(NULL);
-        while(!qu.empty()){
-        	TreeNode* p = qu.front();
-        	qu.pop();
-        	tmp+= p->val;
-        	if(p->left) qu.push(p->left);
-        	if(p->right) qu.push(p->right);
-        	if(qu.front()==NULL){
-        		qu.pop();
-        		if(tick) sumodd+=tmp;
-        		else sumeven+=tmp;
-        		tmp=0;
-        		tick=!tick;
-        		if(!qu.empty())qu.push(NULL);
-        	}
+        pair<int,int> i = helper(r);
+        return max(i.first,i.second);
 
-        }
-        return sumeven>sumodd ? sumeven:sumodd;
     }
+    pair<int,int> helper(TreeNode* p){
+        if(!p) return make_pair(INT_MIN,0);
+        if(!(p->left||p->right)) return make_pair(p->val,0); // so next level if we want to include we use the second
+        int yes, no =0;
+        pair<int,int> l = helper(p->left);
+        pair<int,int> r = helper(p->right);
+        yes = p->val + l.second + r.second;
+        no = max(l.first,l.second) + max(r.first,r.second);
+        // this doesn't work because we need to take empty node into consideration
+        // it will screw up by having normal + INT_MIN like in simple 1,2,null case
+        // at level 1 we have (2,0) yes = 1 no = 2+INT_MIN
+        //no = l.first + r.first;
+        return make_pair(yes,no);
+    }
+
 };
