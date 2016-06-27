d88d263b8e2b0dfaf16c8dd55cc18d9121f264a3
Kevin Wu <xdewtr@gmail.com>
2016-06-27 16:00:33 -0700

    114 wrong when we have input:[1,2,#,3,4,5]

---
+++ 114 - Flatten Binary Tree to Linked List.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void flatten(TreeNode* r) {
+        if(!r) return;
+        TreeNode* p =helper(r);
+    }
+    TreeNode* helper(TreeNode* p){
+    	TreeNode*pre =p, *l=p->left, *r=p->right;
+    	p->left = NULL; p->right =NULL;
+    	if(l){
+    		pre = helper(l);
+    		p->right = l;
+    	}
+    	if(r){
+    		pre->right = r;
+    		pre = helper(r);
+    	}
+    	return p;
+    }
+};
